e920b6289730d9b5c75495c73c3cd33bb44ad31a
Mo Abdelaleem <mohamedkholeef082@gmail.com>
2024-09-16 14:05:48 +0300

    Add lazy loading to some pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,19 +12,21 @@ import { WishlistComponent } from './wishlist/wishlist.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CartComponent } from './cart/cart.component';
 import { OrdersComponent } from './orders/orders.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "home", component: HomeComponent},
-    {path: "products", component: ProductsComponent},
-    { path: "products/:id", component: ProductDetailsComponent },
+    {path: "products", title: 'All Products', loadComponent: () => import('./products/products.component').then(m => m.ProductsComponent) },
+    { path: "products/:id", title: 'Product details', loadComponent: () => import('./product-details/product-details.component').then(m => m.ProductDetailsComponent) },
     {path: "login", canActivate: [unAuthGuard], component: LoginComponent},
     {path: "signup", canActivate: [unAuthGuard], component: SignupComponent},
     {path: "reviews", canActivate: [authGuard], component: ReviewsComponent},
     { path: "reviews/:id", canActivate: [authGuard], component: ReviewUpdateComponent },
     {path: "forgetPassword", canActivate: [unAuthGuard], component: ForgetPasswordComponent},
-    {path: "wishlist", canActivate: [authGuard], component: WishlistComponent},
+    {path: "wishlist", title: 'Wishlist', canActivate: [authGuard], loadComponent: () => import('./wishlist/wishlist.component').then(m => m.WishlistComponent)},
     {path: "cart", canActivate: [authGuard], component: CartComponent},
     {path: "profile", canActivate: [authGuard], component: ProfileComponent},
     {path: "orders", canActivate: [authGuard], component: OrdersComponent},
+    { path: '**', component: NotFoundComponent }
 ];
